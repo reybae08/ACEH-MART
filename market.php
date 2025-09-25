d510af440bdc038f07c2f3c98f69c478d34a2870
reybae08 <reynaldydwiangga@gmail.com>
2025-09-25 10:05:56 +0700

    Logika Pembelian

--- market.php
+++ market.php
@@ -4,4 +4,15 @@ $harga_barang = [35000, 25000, 20000, 21000, 22000];
 
 echo "<h2>-- ACEH MART --</h2>";
 
+
+$beli = [];
+$jumlah = [];
+$total = [];
+$grandtotal = 0;
+$jumlah_item = rand(2, 5);
+$pilih_index = array_rand($nama_barang, $jumlah_item);
+if (!is_array($pilih_index)) {
+    $pilih_index = [$pilih_index];
+}
+
 ?>
